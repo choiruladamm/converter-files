959dedeacc3a7c07f3a2cbd03db8aad09916fa60
choiruladamm <choiruladamm@gmail.com>
2024-04-28 22:47:17 +0700

    chore: add next-themes

---
+++ components/globals/ModeToogle.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface ModeToogleProps {}
+
+const ModeToogle: FC<ModeToogleProps> = ({}) => {
+  return <div>ModeToogle</div>;
+};
+
+export default ModeToogle;
