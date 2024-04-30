7ecf92418b5fdb5342b4b8687c6d78f7853e75a0
choiruladamm <choiruladamm@gmail.com>
2024-04-30 21:29:40 +0700

    feat: wip functionality converter files

--- components/globals/Dropzone.tsx
+++ components/globals/Dropzone.tsx
@@ -1,17 +1,177 @@
 'use client';
 
-import React, { FC, useState } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import ReactDropzone from 'react-dropzone';
 import { LuFileSymlink } from 'react-icons/lu';
-import { FiUploadCloud } from "react-icons/fi";
+import { FiUploadCloud } from 'react-icons/fi';
+import { Action } from '@/types/types';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import loadFfmpeg from '@/utils/loadFfmpeg';
+import convertFile from "@/utils/convert";
 
 interface DropzoneProps {}
 
 const Dropzone: FC<DropzoneProps> = ({}) => {
-  const [isHover, setIsHover] = useState<boolean>(false)
+  const [is_hover, setIsHover] = useState<boolean>(false);
+  const [is_converting, setIsConverting] = useState<boolean>(false);
+  const [is_ready, setIsReady] = useState<boolean>(false);
+  const [is_loaded, setIsLoaded] = useState<boolean>(false);
+  const [is_done, setIsDone] = useState<boolean>(false);
+
+  const [files, setFiles] = useState<Array<any>>([]);
+  const [actions, setActions] = useState<Action[]>([]);
+  const [selcted, setSelected] = useState<string>('...');
+  const [defaultValues, setDefaultValues] = useState<string>('video');
+
+  const ffmpegRef = useRef<any>(null);
+
+  const acceptedFiles = {
+    'image/*': [
+      '.jpg',
+      '.jpeg',
+      '.png',
+      '.gif',
+      '.bmp',
+      '.webp',
+      '.ico',
+      '.tif',
+      '.tiff',
+      '.raw',
+      '.tga',
+    ],
+    'audio/*': [],
+    'video/*': [],
+  };
+
+  const handleUpload = (data: Array<any>): void => {
+    handleExitHover();
+    setFiles(data);
+    const tmp: Action[] = [];
+
+    data.forEach((file: any) => {
+      const formData = new FormData();
+      tmp.push({
+        file_name: file.name,
+        file_size: file.size,
+        from: file.name.slice(((file.name.lastIndexOf('.') - 1) >>> 0) + 2),
+        to: null,
+        file_type: file.type,
+        file,
+        is_converted: false,
+        is_converting: false,
+        is_error: false,
+      });
+    });
+    setActions(tmp);
+  };
+
+  const checkIsReady = useCallback(() => {
+    let tmp_is_ready = true;
+    actions.forEach((action: Action) => {
+      if (!action.to) tmp_is_ready = false;
+    });
+    setIsReady(tmp_is_ready);
+  }, [actions]);
+
+  const load = async () => {
+    const ffmpeg_response: FFmpeg = await loadFfmpeg();
+    ffmpegRef.current = ffmpeg_response;
+    setIsLoaded(true);
+  };
+
+  const reset = () => {
+    setIsDone(false);
+    setActions([]);
+    setFiles([]);
+    setIsReady(false);
+    setIsConverting(false);
+  };
+
+  const downloadAll = (): void => {
+    for (let action of actions) {
+      !action.is_error && download(action);
+    }
+  };
+
+  const download = (action: Action) => {
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = action.url;
+    a.download = action.output;
+
+    document.body.appendChild(a);
+    a.click();
+
+    // Clean up after download
+    URL.revokeObjectURL(action.url);
+    document.body.removeChild(a);
+  };
+
+  const convert = async (): Promise<any> => {
+    let tmp_actions = actions.map((elt) => ({
+      ...elt,
+      is_converting: true,
+    }));
+    setActions(tmp_actions);
+    setIsConverting(true);
+    for (let action of tmp_actions) {
+      try {
+        const { url, output } = await convertFile(ffmpegRef.current, action);
+        tmp_actions = tmp_actions.map((elt) =>
+          elt === action
+            ? {
+                ...elt,
+                is_converted: true,
+                is_converting: false,
+                url,
+                output,
+              }
+            : elt
+        );
+        setActions(tmp_actions);
+      } catch (err) {
+        tmp_actions = tmp_actions.map((elt) =>
+          elt === action
+            ? {
+                ...elt,
+                is_converted: false,
+                is_converting: false,
+                is_error: true,
+              }
+            : elt
+        );
+        setActions(tmp_actions);
+      }
+    }
+    setIsDone(true);
+    setIsConverting(false);
+  };
+
+  const handleHover = (): void => setIsHover(true);
+  const handleExitHover = (): void => setIsHover(false);
+
+  useEffect(() => {
+    if (!actions.length) {
+      setIsDone(false);
+      setFiles([]);
+      setIsReady(false);
+      setIsConverting(false);
+    } else {
+      checkIsReady();
+    }
+  }, [actions, checkIsReady]);
+
+  useEffect(() => {
+    load();
+  }, []);
 
   return (
-    <ReactDropzone>
+    <ReactDropzone
+      onDrop={handleUpload}
+      onDragEnter={handleHover}
+      onDragLeave={handleExitHover}
+      accept={acceptedFiles}
+    >
       {({ getRootProps, getInputProps }) => (
         <div
           {...getRootProps()}
@@ -19,7 +179,7 @@ const Dropzone: FC<DropzoneProps> = ({}) => {
         >
           <input {...getInputProps()} />
           <div className="space-y-4 text-foreground">
-            {isHover ? (
+            {is_hover ? (
               <>
                 <div className="justify-center flex text-6xl">
                   <LuFileSymlink />
