a1251e782001aaa6f4bd0b59a2c0fd7764cb1944
choiruladamm <choiruladamm@gmail.com>
2024-04-30 20:53:21 +0700

    feat: add utils supporting ffmpeg

--- components/globals/Dropzone.tsx
+++ components/globals/Dropzone.tsx
@@ -1,9 +1,48 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useState } from 'react';
+import ReactDropzone from 'react-dropzone';
+import { LuFileSymlink } from 'react-icons/lu';
+import { FiUploadCloud } from "react-icons/fi";
 
 interface DropzoneProps {}
 
 const Dropzone: FC<DropzoneProps> = ({}) => {
-  return <div>Dropzone</div>;
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  return (
+    <ReactDropzone>
+      {({ getRootProps, getInputProps }) => (
+        <div
+          {...getRootProps()}
+          className=" bg-background h-72 lg:h-80 xl:h-96 rounded-3xl shadow-sm border-secondary border-2 border-dashed cursor-pointer flex items-center justify-center"
+        >
+          <input {...getInputProps()} />
+          <div className="space-y-4 text-foreground">
+            {isHover ? (
+              <>
+                <div className="justify-center flex text-6xl">
+                  <LuFileSymlink />
+                </div>
+                <h3 className="text-center font-medium text-2xl">
+                  Yes, right there
+                </h3>
+              </>
+            ) : (
+              <>
+                <div className="justify-center flex text-6xl">
+                  <FiUploadCloud />
+                </div>
+                <h3 className="text-center font-medium text-2xl">
+                  Click, or drop your files here
+                </h3>
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </ReactDropzone>
+  );
 };
 
 export default Dropzone;
